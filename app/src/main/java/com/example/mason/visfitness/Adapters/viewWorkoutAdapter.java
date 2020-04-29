15259d8032367c1ae5661ae98c1915b69bf74cd2
psymg6 <psymg6@nottingham.ac.uk>
2020-04-29 18:26:18 +0100

    Added more QOL improvments

--- app/src/main/java/com/example/mason/visfitness/Adapters/viewWorkoutAdapter.java
+++ app/src/main/java/com/example/mason/visfitness/Adapters/viewWorkoutAdapter.java
@@ -2,11 +2,16 @@ package com.example.mason.visfitness.Adapters;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.mason.visfitness.Models.ExerciseModel;
@@ -20,11 +25,13 @@ import java.util.ArrayList;
  * Created by Mason on 04/12/2019.
  */
 
-public class viewWorkoutAdapter  extends RecyclerView.Adapter<viewWorkoutAdapter.ViewHolder> {
+public class viewWorkoutAdapter  extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
         private ArrayList<RoutinesModel> routineList;
         private LayoutInflater mInflater;
         private Context context;
+        private static final int TYPE_HEADER = 0;
+        private static final int TYPE_ITEM = 1;
 
         public viewWorkoutAdapter(Context context,ArrayList<RoutinesModel> routines) {
             this.context = context;
@@ -33,42 +40,78 @@ public class viewWorkoutAdapter  extends RecyclerView.Adapter<viewWorkoutAdapter
         }
 
         @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = mInflater.inflate(R.layout.recyclerview_workout_item, parent, false);
-            return new ViewHolder(view);
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            if (viewType == TYPE_ITEM) {
+                //inflate your layout and pass it to view holder
+                View view = mInflater.inflate(R.layout.recyclerview_workout_item, parent, false);
+                return new ViewHolderItem(view);
+            } else if (viewType == TYPE_HEADER) {
+                //inflate your layout and pass it to view holder
+                View view = mInflater.inflate(R.layout.layout_header_view_routines, parent, false);
+                return new VHHeader(view);
+            }
+            throw new RuntimeException("there is no type that matches the type " + viewType + " + make sure your using types correctly");
         }
 
-        @SuppressLint("DefaultLocale")
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            RoutinesModel routine = routineList.get(position);
-            String text = String.format("routine: %s", routine.getRoutineName());
-            for(int i=0; i < routine.getExercises().size();i++){
-                ExerciseModel exerciseModel = routine.getExercises().get(i);
-                text=text+String.format("\n%s sets: %d reps: %d",exerciseModel.getExerciseName(),
-                        exerciseModel.getDefaultSets(), exerciseModel.getDefaultReps());
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            if (holder instanceof ViewHolderItem) {
+                int[] drawable_list = new int[]{R.drawable.workout_gradient1, R.drawable.workout_gradient2,
+                        R.drawable.workout_gradient3, R.drawable.workout_gradient4, R.drawable.workout_gradient5};
+                RoutinesModel routine = getItem(position);
+                Drawable drawable = context.getDrawable(
+                        drawable_list[routineList.indexOf(routine) % getItemCount()]);
+                ((ViewHolderItem)holder).frameLayout.setBackground(drawable);
+                ((ViewHolderItem)holder).routine_name.setText(routine.getRoutineName());
+                ((ViewHolderItem)holder).exercise_recycler.setLayoutManager(new LinearLayoutManager(context));
+                WorkoutAdapterExerciseAdapter adapter = new WorkoutAdapterExerciseAdapter(context,
+                        routine.getExercises());
+                ((ViewHolderItem)holder).exercise_recycler.setAdapter(adapter);
             }
-            holder.textView.setText(text);
         }
 
         @Override
         public int getItemCount() {
-            return routineList.size();
+            return routineList.size()+1;
         }
 
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            TextView textView;
+    @Override
+    public int getItemViewType(int position) {
+        if (isPositionHeader(position))
+            return TYPE_HEADER;
+
+        return TYPE_ITEM;
+    }
+    private boolean isPositionHeader(int position) {
+        return position == 0;
+    }
+    private RoutinesModel getItem(int position) {
+        return routineList.get(position - 1);
+    }
+
+    class VHHeader extends RecyclerView.ViewHolder {
+
+        public VHHeader(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public class ViewHolderItem extends RecyclerView.ViewHolder {
+            TextView routine_name;
             ImageView editBtn;
-            ViewHolder(View itemView) {
+            ConstraintLayout frameLayout;
+            RecyclerView exercise_recycler;
+            ViewHolderItem(View itemView) {
                 super(itemView);
-                textView = itemView.findViewById(R.id.textView);
+                routine_name = itemView.findViewById(R.id.routine_name);
                 editBtn = itemView.findViewById(R.id.edit_btn);
-                ViewHolder adapter = this;
+                frameLayout = itemView.findViewById(R.id.frame);
+                exercise_recycler = itemView.findViewById(R.id.exercise_recycler);
                 editBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         MyPopupClass popUpClass = new MyPopupClass();
-                        popUpClass.showPopupWindow(view,routineList.get(getAdapterPosition()),
+                        popUpClass.showPopupWindow(view,getItem(getAdapterPosition()),
                                 context);
                     }
                 });
